41a81ea20154bef9a79fc9bf489a729b5d84a424
lethalbit <201479+lethalbit@users.noreply.github.com>
2025-03-07 19:25:02 +0000

    Deploying to gh-pages from @ shrine-maiden-heavy-industries/sol@8c2e5e7856c2d92658720c930ad5a1b5aa835e84 🚀

--- conf.py
+++ conf.py
@@ -1,13 +1,14 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
-import os, sys
+import os
+import sys
 from pathlib import Path
+
 sys.path.insert(0, os.path.abspath('.'))
 
 from sol_usb import __version__ as sol_version
 
 ROOT_DIR = (Path(__file__).parent).parent
-
 
 project   = 'SOL'
 version   = sol_version
@@ -15,8 +16,6 @@ release   = version.split('+')[0]
 copyright = '2020 Great Scott Gadgets'
 author    = 'Katherine J. Temkin'
 language  = 'en'
-
-
 
 extensions = [
 	'sphinx.ext.autodoc',
@@ -39,7 +38,6 @@ source_suffix = {
 	'.md': 'markdown',
 }
 
-
 pygments_style         = 'monokai'
 autodoc_member_order   = 'bysource'
 graphviz_output_format = 'svg'
@@ -51,11 +49,9 @@ intersphinx_mapping = {
 	'usb_construct': ('https://usb-construct.shmdn.link/', None)
 }
 
-
 napoleon_google_docstring = False
 napoleon_numpy_docstring  = True
 napoleon_use_ivar         = True
-
 
 myst_heading_anchors = 3
 
@@ -76,7 +72,6 @@ html_context = {
 		('latest', '/latest')
 	]
 }
-
 
 html_baseurl     = 'https://sol.shmdn.link/'
 html_theme       = 'sphinx_rtd_theme'
