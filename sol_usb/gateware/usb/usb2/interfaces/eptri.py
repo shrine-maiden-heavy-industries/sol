shrine-maiden-heavy-industries/sol

commit db5cfef32a1ff5e81135c8654d6109c7eed90cc8

refactor: Updated usages of `DomainRenamer`

sol_usb/gateware/usb/usb2/interfaces/eptri.py
@@ -150,7 +150,7 @@ class SetupFIFOInterface(Peripheral, Elaboratable):
 
 		# TODO: generate interrupts
 
-		return DomainRenamer({'sync': 'usb'})(m)
+		return DomainRenamer(sync = 'usb'})(m)
 
 class InFIFOInterface(Peripheral, Elaboratable):
 	''' IN component of our `eptri`-equivalent interface.
@@ -445,7 +445,7 @@ class InFIFOInterface(Peripheral, Elaboratable):
 				with m.If(self.reset.w_stb):
 					m.next = 'IDLE'
 
-		return DomainRenamer({'sync': 'usb'})(m)
+		return DomainRenamer(sync = 'usb')(m)
 
 class OutFIFOInterface(Peripheral, Elaboratable):
 	''' OUT component of our `eptri`
@@ -682,4 +682,4 @@ class OutFIFOInterface(Peripheral, Elaboratable):
 		# Interrupt/status
 		#
 
-		return DomainRenamer({'sync': 'usb'})(m)
+		return DomainRenamer(sync = 'usb')(m)

sol_usb/gateware/debug/ila.py
@@ -161,7 +161,7 @@ class IntegratedLogicAnalyzer(Elaboratable):
 
 		# Convert our sync domain to the domain requested by the user, if necessary.
 		if self.domain != 'sync':
-			m = DomainRenamer(self.domain)(m)
+			m = DomainRenamer(sync = self.domain)(m)
 
 		return m
 
@@ -314,7 +314,7 @@ class SyncSerialILA(Elaboratable):
 
 		# Convert our sync domain to the domain requested by the user, if necessary.
 		if self.domain != 'sync':
-			m = DomainRenamer(self.domain)(m)
+			m = DomainRenamer(sync = self.domain)(m)
 
 		return m
 
@@ -498,7 +498,7 @@ class StreamILA(Elaboratable):
 
 		# Convert our sync domain to the domain requested by the user, if necessary.
 		if self.domain != 'sync':
-			m = DomainRenamer(self.domain)(m)
+			m = DomainRenamer(sync = self.domain)(m)
 
 		return m
 
@@ -589,7 +589,7 @@ class AsyncSerialILA(Elaboratable):
 
 		# Convert our sync domain to the domain requested by the user, if necessary.
 		if self.domain != 'sync':
-			m = DomainRenamer({'sync': self.domain})(m)
+			m = DomainRenamer(sync = self.domain)(m)
 
 		return m
 

sol_usb/gateware/usb/analyzer.py
@@ -106,14 +106,14 @@ class USBAnalyzer(Elaboratable):
 		packet_too_big = Signal()
 
 		# Internal storage
-		m.submodules.data_buffer = data_buffer = DomainRenamer('usb')(
+		m.submodules.data_buffer = data_buffer = DomainRenamer(sync = 'usb')(
 			SyncFIFOBuffered(width = 8, depth = self.mem_size)
 		)
-		m.submodules.packet_buffer = packet_buffer = DomainRenamer('usb')(
+		m.submodules.packet_buffer = packet_buffer = DomainRenamer(sync = 'usb')(
 			SyncFIFOBuffered(width = 8, depth = USBAnalyzer.MAX_PACKET_SIZE_BYTES)
 		)
 		# The top bit of the length buffer is an overrun flag - if set, we've had an overrun occur in the primary FIFO.
-		m.submodules.length_buffer = length_buffer = DomainRenamer('usb')(
+		m.submodules.length_buffer = length_buffer = DomainRenamer(sync = 'usb')(
 			SyncFIFOBuffered(width = packet_length.width + 1, depth = 256)
 		)
 

commit 34b51b47261edd6afd2cca27a773085e439b2287

usb2.interfaces.eptri: Fixed a syntax error that was introduced in db5cfef
This wasn't found because our test coverage is pretty damn poor, oops.

sol_usb/gateware/usb/usb2/interfaces/eptri.py
@@ -150,12 +150,12 @@ class SetupFIFOInterface(Peripheral, Elaboratable):
 
 		# TODO: generate interrupts
 
-		return DomainRenamer(sync = 'usb'})(m)
+		return DomainRenamer(sync = 'usb')(m)
 
 class InFIFOInterface(Peripheral, Elaboratable):
 	''' IN component of our `eptri`-equivalent interface.
 
-	Implements the FIFO that handles `eptri` IN requests. This FIFO collects USB data, and
+		Implements the FIFO that handles `eptri` IN requests. This FIFO collects USB data, and
 	transmits it in response to an IN token. Like all `eptri` interfaces; it can handle only one
 	pending packet at a time.
 